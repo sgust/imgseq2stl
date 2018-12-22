f28775b9cebfb9d9a2354185cf65b87c7e8105ae
Soeren Gust <sgust@ithh.informationstheater.de>
2018-12-22 15:25:20 +0100

    Simple script to show the bounding box of a ASCII STL file.

---
+++ boundingbox.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+my $xmin = 1e6;
+my $ymin = 1e6;
+my $zmin = 1e6;
+
+my $xmax = -1;
+my $ymax = -1;
+my $zmax = -1;
+
+while(<>) {
+	if (/vertex\s+([0-9.efEF]+)\s+([0-9efEF]+)\s+([0-9efEF]+)/) {
+		if ($1 < $xmin) { $xmin = $1; }
+		if ($2 < $ymin) { $ymin = $2; }
+		if ($3 < $zmin) { $zmin = $3; }
+		if ($1 > $xmax) { $xmax = $1; }
+		if ($2 > $ymax) { $ymax = $2; }
+		if ($3 > $zmax) { $zmax = $3; }
+	}
+}
+
+print "X ", $xmin, "-", $xmax, "\n";
+print "Y ", $ymin, "-", $ymax, "\n";
+print "Z ", $zmin, "-", $zmax, "\n";
